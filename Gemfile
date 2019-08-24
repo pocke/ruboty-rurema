25603c64bf1d75414abae0925b6c9535503c57dc
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-08-24 16:10:21 +0900

    bunlde gem

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in ruboty-rurema.gemspec
+gemspec
+
+gem "rake", "~> 12.0"
---
+++ lib/ruboty/rurema.rb
@@ -0,0 +1,8 @@
+require "ruboty/rurema/version"
+
+module Ruboty
+  module Rurema
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
